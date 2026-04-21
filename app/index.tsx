8dd269f291a2d2d1e81b1d7115d83a12aca683b9
André Souza <andreloff15@gmail.com>
2026-04-21 19:51:29 -0300

    changed initial screen on app startup to home

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  const isLogged = false;
+  const isLogged = true;
 
   if (isLogged) {
     return <Redirect href="(app)/homeScreen" />;
