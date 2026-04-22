2916e54b8d6d2d48e5b5100ce8578170259ed0c2
André Souza <andreloff15@gmail.com>
2026-04-21 23:00:42 -0300

    updated drawer layout file to use new custom component

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,12 +1,14 @@
+import CustomDrawer from '@/components/customDrawer/CustomDrawer';
 import { Drawer } from 'expo-router/drawer';
 
 export default function DrawerLayout() {
   return (
-    <Drawer screenOptions={{ headerShown: false }}>
+    <Drawer 
+      screenOptions={{ headerShown: false }}
+      drawerContent={(props) => <CustomDrawer {...props} />}
+    >
       <Drawer.Screen name="(home)" options={{ title: 'Home' }} />
       <Drawer.Screen name="(profile)" options={{ title: 'Profile' }} />
     </Drawer>
   );
 }
-
-// <Drawer.Screen name="index" options={{ title: 'Home' }} />
