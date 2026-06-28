c5800ab4892400d23142a159dde0f600ca29390d
andreloff <andreloff15@gmail.com>
2026-06-28 13:59:27 -0300

    botao de "Sair" do drawer executa logoff do usuário

--- components/customDrawer/DrawerFooter.tsx
+++ components/customDrawer/DrawerFooter.tsx
@@ -1,9 +1,29 @@
+import { useRouter } from "expo-router";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { auth } from "../../src/services/firebaseConfig";
 
 export default function DrawerFooter() {
+
+  const router = useRouter();
+  
+  const handleLogoutPress = async () => {
+    if (auth.currentUser) {
+      try{
+        router.replace("/loginScreen");
+        await auth.signOut();
+        console.log("Logoff com sucesso!");
+      } catch (error) {
+        console.error("Erro no logoff: ", error);
+      }
+    } 
+  };
+
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button}>
+      <Pressable 
+        style={styles.button}
+        onPress={handleLogoutPress}
+      >
         <Text style={styles.buttonText}>Sair</Text>
       </Pressable>
     </View>
